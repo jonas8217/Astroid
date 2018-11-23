559a979b83d8eb7da88abce7503b58fcb4edfc05
jonas8217 <Jonas82173000@gmail.com>
2018-11-23 21:32:18 +0100

    it should be fine now...

--- highscoreLogger.py
+++ highscoreLogger.py
@@ -6,11 +6,7 @@ class Logger:
     def __init__(self):
         self.handler = Handler()
 
-<<<<<<< HEAD
-    def post_score(self,game,score,opt1="n/a",opt2="n/a",opt3="n/a"):
-=======
     def post_score(self, game, score, opt1="n/a", opt2="n/a", opt3="n/a"):
->>>>>>> c3467e82b22876056a3cc4c155f06a4a4e7c1486
         #Poster score til game
         #Returnerer liste af scores til game
         scores = self.handler.update(game, score, opt1, opt2, opt3)
@@ -20,10 +16,6 @@ class Logger:
                 templist.append(s)
 
         return templist
-<<<<<<< HEAD
-
-=======
->>>>>>> c3467e82b22876056a3cc4c155f06a4a4e7c1486
 
     def get_scores(self, game):
         #Poster score til game
